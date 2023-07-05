9022ca2efcf32d5d5e8e0395e6e1525841ea0da5
kardelengokce <kardelengokce.88@gmail.com>
2023-07-05 10:19:48 +0300

    settings

--- medium1/medium1/asgi.py
+++ medium1/medium1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mediumblog project.
+ASGI config for medium1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediumblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medium1.settings')
 
 application = get_asgi_application()
--- medium1/medium1/wsgi.py
+++ medium1/medium1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mediumblog project.
+WSGI config for medium1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediumblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medium1.settings')
 
 application = get_wsgi_application()
